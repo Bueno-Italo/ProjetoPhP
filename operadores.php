0802c73bc9fd37ae20b8015969ef25efbb8ef020
italo <italo-bueno@hotmail.com>
2024-07-10 21:43:04 -0300

    trabalhando com operadores

---
+++ operadores.php
@@ -0,0 +1,8 @@
+<?php
+    //Operadores de Atruibuição! Adição, subtração, multiplicação, divisão, modulo e concatenação
+    $preco = $_GET['p'];
+    echo "O preço do produto é R$ $preco";
+
+    echo 'teste';
+    
+?>
