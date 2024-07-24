c3e1875d068d9d7c1b501fae4fa00832f041cdcc
italo <italo-bueno@hotmail.com>
2024-07-24 12:16:49 -0300

    switch

---
+++ switch.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <h2>Ler um numero e qual operação realizar, dobro, cubo ou raiz...</h2>
+            <?php
+                echo"ola";
+            ?>
+        </div>
+    </body>
+</html>
