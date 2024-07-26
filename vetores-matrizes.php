dcc7dbd1183920504e0060d41126339ce612e82c
italo <italo-bueno@hotmail.com>
2024-07-26 10:42:23 -0300

    atirbuindo vetores

---
+++ vetores-matrizes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <pre>
+            <h2>Trabalhando com Vetores e Matrizes</h2>
+
+
+
+            <?php
+                $V = array();
+
+
+
+            ?>
+
+            </pre>
+        </div>
+    </body>
+</html>
+
+
+
+
+
