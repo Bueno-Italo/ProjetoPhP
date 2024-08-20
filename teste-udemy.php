1445378c60d87d78e63598b871d295e78b62474a
italo <italo-bueno@hotmail.com>
2024-08-20 19:14:20 -0300

    Teste documento php

---
+++ teste-udemy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+    <head>
+        <title>
+            Teste Aula
+        </title>
+    </head>
+    <body>
+        <h1></h1>
+    </body>
+
+    <?php
+        echo"Teste";
+    ?>
+
+</html>
